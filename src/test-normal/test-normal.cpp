02aaa928b1df92479bf97e9689bd7762c2741e55
liuqi <night_fightina@163.com>
2020-03-07 19:42:02 +0800

    modify README.md

---
+++ src/test-normal/test-normal.cpp
@@ -0,0 +1,25 @@
+#include <google/profiler.h> 
+
+void test1() {
+    int i = 0;
+    while (i < 1000) i++;
+}
+
+void test2() {
+    int i = 0;
+    while (i < 2000) i++;
+}
+
+void test3() {
+    for (int i = 0; i < 100000; ++i){
+        test1();
+        test2();
+    }
+}
+
+int main() {
+     ProfilerStart("test.prof"); // test.prof is the name of profile file
+    test3();
+     ProfilerStop();
+    return 0;
+}
---
+++ src/test-server/test-server.cpp
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <signal.h>
+
+#include <google/profiler.h>
+
+//SIGUSR1: start profiling
+//SIGUSR2: stop profiling
+
+static void gprof_callback(int signum) {
+    if (signum == SIGUSR1) {
+        printf("Catch the signal ProfilerStart\n");
+        ProfilerStart("bs.prof");
+    }
+    else if (signum == SIGUSR2) {
+        printf("Catch the signal ProfilerStop\n");
+        ProfilerStop();
+    }
+}
+
+static void setup_signal() {
+    struct sigaction profstat;
+    profstat.sa_handler = gprof_callback;
+    profstat.sa_flags = 0;
+    sigemptyset(&profstat.sa_mask);                                        
+    sigaddset(&profstat.sa_mask, SIGUSR1);
+    sigaddset(&profstat.sa_mask, SIGUSR2);
+
+    if (sigaction(SIGUSR1, &profstat,NULL) < 0) 
+        fprintf(stderr, "SIGUSR1 Fail !");
+
+    if (sigaction(SIGUSR2, &profstat,NULL) < 0) 
+        fprintf(stderr, "SIGUSR2 Fail !");
+}
+
+int loopop_callee() {
+    int n=0;
+    for(int i=0; i<10000; i++) 
+        for(int j=0; j<10000; j++)
+            n |= i%100 + j/100;
+    return n;
+}
+
+int loopop() {
+    int n=0;
+    while(1) {
+        for(int i=0; i<10000; i++) 
+            for(int j=0; j<10000; j++) 
+                n |= i%100 + j/100;
+        printf("result:  %d\n", (loopop_callee)() );
+    }
+    return n;
+}
+
+int main(int argc,char** argv) {
+    setup_signal();
+    printf("result:  %d\n", (loopop)() );
+    return 0;
+}
